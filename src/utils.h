099ef599d833c7d50b481da7ed9146ae68b6bbdf
reggna <230158+reggna@users.noreply.github.com>
2026-04-03 16:53:01 -0700

    Change default log from CLIENTERROR to CLIENTNOTICE

--- src/utils.h
+++ src/utils.h
@@ -1,5 +1,16 @@
 #include <common.h>
 #include <core/levels.h>
 #include <fe-common/core/printtext.h>
-#define printf(...) printtext(NULL, NULL, MSGLEVEL_CLIENTERROR, __VA_ARGS__)
+
+static inline void discord_printf(const char *fmt, ...) {
+	va_list args;
+	va_start(args, fmt);
+	char *str = g_strdup_vprintf(fmt, args);
+	va_end(args);
+
+	printtext(NULL, NULL, MSGLEVEL_CLIENTNOTICE, "%s", str);
+	g_free(str);
+}
+
+#define printf(...) discord_printf(__VA_ARGS__)
 #define debug() printf("======%s:%d %s======", __FILE__, __LINE__, __func__)
